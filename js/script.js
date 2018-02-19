ac64f081b24e6fdd8b610c29eecc2b29e037aace
Agata Czerwinska <agata@rubrica.pl>
2018-02-19 14:54:18 +0100

    Add scrolling to potfolio section

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+    $("#scroll-button").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#portfolio-section").offset().top
+        }, 1500);
+    });
+});
